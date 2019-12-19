c4c275ed9e1ff9bceccd25776ede51ddec358a7d
Giovanni Quattrocchi <giovanniquattrocchi@me.com>
2019-12-19 10:15:58 +0100

    ex10

--- ios/SwiftIntro.playground/Contents.swift
+++ ios/SwiftIntro.playground/Contents.swift
@@ -13,6 +13,7 @@ for c in hi {
     print(c)
 }
 
+
 hi.append("???")
 
 var greet: String? = "Hello"
@@ -68,9 +69,63 @@ func add1AndApply(_ n: Int, f: (Int) -> Int) -> Int {
     return f(n+1)
 }
 
+add1AndApply(5, f: square)
 
 add1AndApply(5, f: { $0*$0*$0 })
+add1AndApply(5, f: { x in x*x*x})
+add1AndApply(5, f: { (x: Int) in x*x*x})
 
 let r = 1...10
+let r1 = 1..<10
 
 r.map(square)
+
+var res = r.filter({ $0 % 2 == 0})
+res
+
+var res2 = r.reduce(0, { $0 + $1 })
+
+res2
+
+protocol HasSpeed {
+    var speed: Int {get set}
+    mutating func turnOn()
+}
+
+class Car: HasSpeed {
+    var speed = 0
+    func turnOn() {
+        if speed == 0 {
+            speed = 1
+        }
+    }
+}
+
+let car1 = Car()
+car1.turnOn()
+car1.speed += 5
+
+car1.speed
+
+// car1 = Car()
+
+struct Car2 : HasSpeed {
+    var speed = 0
+    
+    mutating func turnOn(){
+        if speed == 0 {
+            speed = 1
+        }
+    }
+}
+
+var car2 = Car2()
+let car3 = car2
+car2.turnOn()
+car2.speed
+car3.speed
+
+var car4 = Car()
+var car5 = car4
+car4.turnOn()
+car5.speed
